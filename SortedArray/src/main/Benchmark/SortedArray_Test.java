555f10f93d2139cae518893a64b7d4b566bf66fa
Pavel Urvantcev <pavel.urvantcev@gmail.com>
2023-09-07 17:42:31 +0200

    SortedArray done

--- SortedArray/src/main/Benchmark/SortedArray_Test.java
+++ SortedArray/src/main/Benchmark/SortedArray_Test.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 
 class SortedArray_Test {
-
     static final int samples = 10_000;
-    static int[] sizes = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 5000, 10_000};//, 20_000, 50_000, 100_000};
+    static int[] sizes = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 5000, 10_000, 20_000};//, 50_000, 100_000};
     static int[][] data1 = new int[sizes.length][];
     static int[][] data2 = new int[sizes.length][];
     static int[][] keys = new int[sizes.length][];
     static ArrayList<Long>[] linResults = new ArrayList[sizes.length];
     static ArrayList<Long>[] binResults = new ArrayList[sizes.length];
+    static ArrayList<Long>[] advanceResults = new ArrayList[sizes.length];
 
 
     public static void main(String[] arg) {
@@ -34,6 +34,7 @@ class SortedArray_Test {
         for (int i = 0; i < linResults.length; i++) {
             linResults[i] = new ArrayList<Long>(samples);
             binResults[i] = new ArrayList<Long>(samples);
+            advanceResults[i] = new ArrayList<Long>(samples);
         }
     }
 
@@ -75,6 +76,9 @@ class SortedArray_Test {
         for (ArrayList<Long> result : binResults) {
             result.clear();
         }
+        for (ArrayList<Long> result : advanceResults) {
+            result.clear();
+        }
     }
 
     static void run() {
@@ -83,8 +87,9 @@ class SortedArray_Test {
         for (int i = 0; i < sizes.length; i++) {
             //bench(keys[i], i,Inst.Lin);
             //bench(keys[i], i,Inst.Bin);
-            bench(keys[i], i,Inst.LinDupe);
-            bench(keys[i], i,Inst.BinDupe);
+            bench(keys[i], i, Inst.LinDupe);
+            bench(keys[i], i, Inst.BinDupe);
+            bench(keys[i], i, Inst.AdvanceDupe);
         }
 
         clear();
@@ -94,14 +99,14 @@ class SortedArray_Test {
         System.out.printf("#%10s%15s%15s%15s%15s\n", "N", "Fastest", "Slowest", "Average", "Median");
         for (int i = 0; i < sizes.length; i++) {
             bench(keys[i], i,Inst.Lin);
-            data(linResults[i], i);
+            data(linResults[i], i, samples);
         }*/
 
         /*System.out.println("\nBinary");
         System.out.printf("#%10s%15s%15s%15s%15s\n", "N", "Fastest", "Slowest", "Average", "Median");
         for (int i = 0; i < sizes.length; i++) {
             bench(keys[i], i,Inst.Bin);
-            data(binResults[i], i);
+            data(binResults[i], i,samples);
         }*/
 
 
@@ -109,7 +114,7 @@ class SortedArray_Test {
         System.out.printf("#%10s%15s%15s%15s%15s\n", "N", "Fastest", "Slowest", "Average", "Median");
         for (int i = 0; i < sizes.length; i++) {
             bench(keys[i], i, Inst.LinDupe);
-            data(linResults[i], i);
+            data(linResults[i], i, 1);
         }
 
 
@@ -117,7 +122,14 @@ class SortedArray_Test {
         System.out.printf("#%10s%15s%15s%15s%15s\n", "N", "Fastest", "Slowest", "Average", "Median");
         for (int i = 0; i < sizes.length; i++) {
             bench(keys[i], i, Inst.BinDupe);
-            data(binResults[i], i);
+            data(binResults[i], i, 1);
+        }
+
+        System.out.println("\nAdvance Dupe");
+        System.out.printf("#%10s%15s%15s%15s%15s\n", "N", "Fastest", "Slowest", "Average", "Median");
+        for (int i = 0; i < sizes.length; i++) {
+            bench(keys[i], i, Inst.AdvanceDupe);
+            data(advanceResults[i], i, 1);
         }
     }
 
@@ -147,7 +159,7 @@ class SortedArray_Test {
                 case LinDupe:
                     t0 = System.nanoTime();
                     for (int item : data1[i]) {
-                       SortedArray.searchUnsorted(data2[i], item);
+                        SortedArray.searchUnsorted(data2[i], item);
                     }
                     t1 = System.nanoTime();
 
@@ -155,18 +167,41 @@ class SortedArray_Test {
                 case BinDupe:
                     t0 = System.nanoTime();
                     for (int item : data1[i]) {
-                       SortedArray.binarySearch(data2[i], item);
+                        SortedArray.binarySearch(data2[i], item);
                     }
                     t1 = System.nanoTime();
 
                     binResults[i].add(t1 - t0);
+                case AdvanceDupe:
+                    int x = 0;
+                    int y = 0;
+
+                    t0 = System.nanoTime();
+                    while (x < data1[i].length && y < data2[i].length) {
+                        int item1 = data1[i][x];
+                        int item2 = data2[i][y];
+
+                        if (item1 < item2) {
+                            x++;
+                            continue;
+                        }
+                        if (item1 > item2) {
+                            y++;
+                            continue;
+                        }
+                        x++;
+                        y++;
+                    }
+                    t1 = System.nanoTime();
+
+                    advanceResults[i].add(t1 - t0);
             }
             runs--;
         }
     }
 
 
-    static void data(ArrayList<Long> list, int i) {
+    static void data(ArrayList<Long> list, int i, int div) {
         long sum = 0;
         double avg = 0;
 
@@ -179,17 +214,18 @@ class SortedArray_Test {
 
         System.out.printf("#%10d%15.3f%15.3f%15.3f%15.3f\n",
                 sizes[i],
-                list.get(0) / (double) samples,
-                list.get(list.size() - 1) / (double) samples,
-                avg / (double) samples,
-                list.get((int) (list.size() / 2.0 + 0.5)) / (double) samples);
+                list.get(0) / (double) div,
+                list.get(list.size() - 1) / (double) div,
+                avg / (double) div,
+                list.get((int) (list.size() / 2.0 + 0.5)) / (double) div);
     }
 
     enum Inst {
         Lin,
         Bin,
         LinDupe,
-        BinDupe
+        BinDupe,
+        AdvanceDupe
 
     }
 }
